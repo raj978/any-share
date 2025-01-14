705a8681343d60b4153543109ef7fb79e9754958
Rajat Mahesh Gupta <guptarajat978@gmail.com>
2025-01-14 08:31:42 -0800

    About page

--- app/(dashboard)/about/page.js
+++ app/(dashboard)/about/page.js
@@ -1,19 +1,19 @@
 "use client";
 import React from "react";
 // Replace the react-icons import with lucide-react
-import { BrandGithub, ExternalLink } from "lucide-react";
+import { ExternalLink } from "lucide-react";
 
 const AboutPage = () => {
   return (
-    <div className="min-h-screen bg-background text-foreground flex items-center justify-center p-5">
+    <div className="min-h-screen bg-background text-foreground flex  justify-center p-5">
       <div className="max-w-3xl text-center">
         <h1 className="text-4xl font-bold mb-6">About Any-Share</h1>
         <p className="text-lg mb-6">
-          **Any-Share** is a secure and efficient file-sharing platform that
-          allows you to easily upload, manage, and share your files with others.
-          Built with modern technologies like Next.js, Firebase, Tailwind CSS,
-          and Clerk for seamless authentication, Any-Share ensures your files
-          are always accessible and protected.
+          Any-Share is a secure and efficient file-sharing platform that allows
+          you to easily upload, manage, and share your files with others. Built
+          with modern technologies like Next.js, Firebase, Tailwind CSS, and
+          Clerk for seamless authentication, Any-Share ensures your files are
+          always accessible and protected.
         </p>
         <p className="text-lg mb-6">
           Whether you're sharing documents, images, or any other type of file,
@@ -37,7 +37,12 @@ const AboutPage = () => {
             rel="noopener noreferrer"
             className="text-foreground hover:text-primary transition text-2xl"
             aria-label="View GitHub Repository">
-            <BrandGithub />
+            <img
+              height={25}
+              width={25}
+              src="/github-mark-white.png"
+              alt="GitHub Repository"
+            />
           </a>
         </div>
       </div>
